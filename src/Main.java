64bfa9648d1ce7c5d8c54cf35bac5fbdb0f9fd9c
Fredrick Kristoffersson <08_krogen.delar@icloud.com>
2024-10-08 14:21:21 +0200

    Here is a summary: I have added files and used the blueprint from the task. Trying back and forth with where to place discounts and such. Used AI to get an understanding for structure asked questions to help me understand the code better. Did have some problem with BaseDiscount being an abstract class but have a better understanding now.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        //Product snusEttanLös = new Product("Ettan lös", 69, 1102);
+        Product snusEttanLös = new Product("Ettan lös", 69, 1102);
         Product mjölk = new Product("Mjölk", 28, 47);
         Product bröd = new Product("Bröd", 33, 12);
 
@@ -8,7 +8,7 @@ public class Main {
                 new MilkDiscount(
                         new QuantityDiscount(null)));
 
-        //System.out.println(snusEttanLös.toString());
+        System.out.println(snusEttanLös.toString());
 
         double milkDiscount = discountChain.apply(mjölk);
         String milkDescription = discountChain.getDescription(mjölk);
